564280a4c501c0e653364e54789ed1ea78ba0428
koji149 <koji06099@gmail.com>
2020-06-21 01:20:59 +0900

    seo対策

--- app/controllers/api_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApiController < ApplicationController
-  def success
-    render status: 200, json: { status: 200, message: "Success" }
-  end
-end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'アフターキャンパス',
+      title: 'アフターキャンパス',
+      reverse: true,
+      charset: 'utf-8',
+      description: '「飲みに行こう！」と駅に集まり、「どこ行く？」「いつものとこで良くない？」「安いところ?」結局いつもと同じ居酒屋...そんな悩みを解決する予算3000円で楽しめる大学生専用飲み会サイト！',
+      keywords: '居酒屋, 居酒屋 安い, 大学生 オススメ 居酒屋, 飲み放題 居酒屋, 近く の 居酒屋',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('toplogo.png') },
+        { href: image_url('toplogo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: :site, # もしくは site_name: :site
+        title: :title, # もしくは title: :title
+        description: :description, # もしくは description: :description
+        type: 'website',
+        url: request.original_url,
+        image: image_url('toplogo.png'),
+        locale: 'ja_JP',
+      },
+      #twitter: {
+        #card: 'summary',
+        #site: '@ツイッターのアカウント名',
+      #}
+    }
+  end
 end
