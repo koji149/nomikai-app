c7d34645f1eb8dd513377cea844856b66000a09f
koji149 <koji06099@gmail.com>
2020-06-24 22:24:01 +0900

    sitemapを参照するよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount_roboto
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz")
+  get '/sitemap', to: redirect("https://#{ENV['S3_BUCKET_NAME']}.s3-ap-northeast-1.amazonaws.com/sitemap.xml.gz")
   root 'bars#top'
   get 'bars/index' => 'bars#index'
-
 end
