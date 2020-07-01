36d491e4893272972fa1a4b9f57a1c018655d2b9
koji149 <koji06099@gmail.com>
2020-07-01 23:54:42 +0900

    現在地取得検証

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -60,12 +60,59 @@ class BarsController < ApplicationController
     g_uri = ENV['GURI']
     g_key = ENV['PRO_GKEY']
 
-    clnt = HTTPClient.new
-    body = {
-      key: g_key,
+
+
+    @lat = param["data"]["latitude"]
+  @lon = params["data"]["longitude"]
+
+
+    if params[:genre]
+      @range = 2
+      @area_name = params[:area_name]
+      @category = params[:category]
+      @genre = params[:genre]
+      @budget = params[:budget]
+      @wifi = params[:wifi]
+      @non_smoking = params[:non_smoking]
+      @explain = params[:explain]
+      @free_drink = params[:free_drink]
+    else
+      @range = 5
+      @area_name = params[:area_name]
+      @category = params[:category]
+      @genre = "G001,G002,G012"
+      @budget = "B001,B002"
+      @wifi = 0
+      @non_smoking = 0
+      @explain = params[:explain]
+      @free_drink = 1
+    end
+
+    data = {
+      key: key,
+      lat: @lat,
+      lng: @lng,
+      range: @range,
+      free_drink: @free_drink,
+      budget: @budget,
+      genre: @genre,
+      wifi: @wifi,
+      non_smoking: @non_smoking,
+      format: "json",
+      count: 100
     }
-    res = clnt.post(g_uri, body)
-    @res = JSON.parse(res.body)
+    client = HTTPClient.new
+    request =  client.get('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/', data)
+    @response = JSON.parse(request.body)
+    @items = @response["results"]["shop"]
+    @sum_items = @response["results"]["shop"].length
+    @first_item = @response["results"]["shop"].first
+    if @items.empty?
+      @explain = "近くのお店が見つかりませんでした。。。\nトップページから地域を選択するか、場所を移動して再実行してください"
+    else
+      @index_description = "#{@first_item["name"]}、予算#{@first_item["budget"]["name"]}、飲み放題#{@first_item["free_drink"].slice(0,2)}、#{@first_item["mobile_access"]}...他#{@sum_items}件"
+      @items = Kaminari.paginate_array(@items).page(params[:page]).per(12)
+    end
 
   end
 
