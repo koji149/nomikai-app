bfcd8b5c0a607de5aee8032f9d25e290071e346c
koji149 <koji06099@gmail.com>
2020-07-11 22:12:17 +0900

    meetingモデル修正

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,11 +3,11 @@ class Meeting < ApplicationRecord
 
   has_one_attached :image
 
-  validates :area, presence: { message: 'は１文字以上入力してください。' },
-  validates :date_time, presence: { message: 'は１文字以上入力してください。' },
+  validates :area, presence: { message: 'は１文字以上入力してください。' }
+  validates :date_time, presence: { message: 'は１文字以上入力してください。' }
   validates :bar, presence: { message: 'は１文字以上入力してください。' }, length: { maximum: 50 }
   validates :url, presence: { message: 'は１文字以上入力してください。' }, length: { maximum: 100 }
-  validates :explain , presence: { message: 'は１文字以上入力してください。' }
+  validates :explain, presence: { message: 'は１文字以上入力してください。' }
 
 
   #enum area: {
