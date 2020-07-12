9c50b8745a156ce75fbda27af1b4e9e1d756ec98
koji149 <koji06099@gmail.com>
2020-07-13 00:31:47 +0900

    redirect先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:success]
-    user_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     flash[:success]
-    user_path
+    user_path(resource)
   end
 
   private
