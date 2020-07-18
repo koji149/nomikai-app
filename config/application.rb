902754f16df3f1b9aac6a819c7551b08c525ab56
koji149 <koji06099@gmail.com>
2020-07-18 23:56:48 +0900

    modalをshowからhideに変更

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module NomikaiApp
   module ChatSpace
     class Application < Rails::Application
       config.i18n.default_locale = :ja
-      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     end
   end
 end
