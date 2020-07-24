7fb7c75afb8fb26981921c552afdd3eb14d88f60
koji149 <koji06099@gmail.com>
2020-07-24 23:38:20 +0900

    index修正

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -15,9 +15,7 @@ class MeetingsController < ApplicationController
     if params[:latitude].present? && params[:longitude].present?
       current_lat = params[:latitude]
       current_lng = params[:longitude]
-      
-      @meeting = Meeting.first.within(3, origin: [current_lat, current_lng]).order(updated_at: :desc).page(params[:page]).per(12)
-      @meetings = Meeting.drop(1).within(3, origin: [current_lat, current_lng]).order(updated_at: :desc).page(params[:page]).per(12)
+      @meetings = Meeting.all.within(3, origin: [current_lat, current_lng]).order(updated_at: :desc).page(params[:page]).per(12)
       @sum_meetings = @meetings.length
       @area_name = "近くの募集一覧"
     elsif params[:area]
@@ -33,12 +31,10 @@ class MeetingsController < ApplicationController
           @area_name = "福岡の募集一覧"
         else
       end
-      @meeting = Meeting.first.where(area: area_num).order(updated_at: :desc).page(params[:page]).per(12)
-      @meetings = Meeting.drop(1).where(area: area_num).order(updated_at: :desc).page(params[:page]).per(12)
+      @meetings = Meeting.where(area: area_num).order(updated_at: :desc).page(params[:page]).per(12)
       @sum_meetings = @meetings.length
     else
-      @meetings = Meeting.first.order(updated_at: :desc).page(params[:page]).per(12)
-      @meetings = Meeting.drop(1).order(updated_at: :desc).page(params[:page]).per(12)
+      @meetings = Meeting.all.order(updated_at: :desc).page(params[:page]).per(12)
       @area_name = "全募集一覧"
       @sum_meetings = @meetings.length
     end
