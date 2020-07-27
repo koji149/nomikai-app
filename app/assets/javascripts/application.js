b62079a893917e31383d8bc869aebe953017529b
koji149 <koji06099@gmail.com>
2020-07-27 18:30:24 +0900

    js修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require popper
-//= require bootstrap-sprockets
